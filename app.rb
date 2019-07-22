29149565d3dcbbd648c8f54e517ca5917949556c
Gavin Kimberlin and Joshua Horgen <gavin@>
2019-07-22 16:02:43 -0700

    Add html

--- app.rb
+++ app.rb
@@ -1,7 +1,34 @@
 require('sinatra')
 require('sinatra/reloader')
+require('./lib/album')
+require('pry')
 also_reload('lib/**/*.rb')
 
+get('/') do
+  @albums = Album.all
+  erb(:albums)
+end
+
+get('/albums') do
+  @albums = Album.all
+  erb(:albums)
+end
+
+get ('/albums/new') do
+  erb(:new_album)
+end
+
+post ('/albums') do
+  binding.pry
+end
+
+post ('/albums') do
+  name = params[:album_name]
+  album = Album.new(name, nil)
+  album.save()
+  @albums = Album.all() # Adding this line will fix the error.
+  erb(:albums)
+end
 
 get ('/') do
   "This will be our home page. '/' is always the root route in a Sinatra application."
